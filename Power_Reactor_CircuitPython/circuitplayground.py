c95e29b92be7a354f8c6d10e85e99b3dd309cc92
jteets <jteets@int.coocoo.com>
2017-10-31 19:35:15 -0400

    Update to work with latest Circuit Python & libraries
    Tested with the following:
    Circuit Python 2.1.0
    adafruit-circuitpython-bundle-2.1.0-20171019.zip
    
    Looked at generating mpy files, seems it's involved and has changed
    as Circuit Python has evolved, so only checking these in for now.

--- Power_Reactor_CircuitPython/circuitplayground.py
+++ Power_Reactor_CircuitPython/circuitplayground.py
@@ -1,15 +1,15 @@
 import board
-import nativeio
+import digitalio 
 
 import neopixel
 
 
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 10)
-left_button = nativeio.DigitalInOut(board.LEFT_BUTTON)
-left_button.switch_to_input(pull=nativeio.DigitalInOut.Pull.DOWN)
+left_button = digitalio.DigitalInOut(board.BUTTON_A)
+left_button.switch_to_input(pull=digitalio.Pull.DOWN)
 
-right_button = nativeio.DigitalInOut(board.RIGHT_BUTTON)
-right_button.switch_to_input(pull=nativeio.DigitalInOut.Pull.DOWN)
+right_button = digitalio.DigitalInOut(board.BUTTON_B)
+right_button.switch_to_input(pull=digitalio.Pull.DOWN)
 
-slide_switch = nativeio.DigitalInOut(board.SLIDE_SWITCH)
-slide_switch.switch_to_input(pull=nativeio.DigitalInOut.Pull.UP)
+slide_switch = digitalio.DigitalInOut(board.SLIDE_SWITCH)
+slide_switch.switch_to_input(pull=digitalio.Pull.UP)
